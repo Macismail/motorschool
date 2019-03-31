e130ea6721585b20f262b8bc1d2ad8cbcea1e781
macismail <samouel_77@hotmail.com>
2019-03-31 16:57:34 +0100

    edit instructor page

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,5 +1,5 @@
 class InstructorsController < ApplicationController
-  # before_action :set_instructor, only: %i[show edit update destroy]
+  before_action :set_instructor, only: %i[show edit update destroy]
 
   # GET /instructors
   # GET /instructors.json
