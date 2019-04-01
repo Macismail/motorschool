ee5cd322eb24754823c33c9c8742a4bb81880e89
macismail <samouel_77@hotmail.com>
2019-04-01 22:42:03 +0100

    update user

--- db/migrate/20190401211109_update_user.rb
+++ db/migrate/20190401211109_update_user.rb
@@ -1,6 +1,6 @@
 class UpdateUser < ActiveRecord::Migration[5.2]
   def change
-    @usr = User.find_by(email: 'admin@email.ie')
+    @usr = User.find_by(email: 'admin@gmail.com')
     @usr.update_attribute :admin, true
   end
 end
