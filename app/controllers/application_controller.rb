dcc37e296ecdd59e42447e456246692dca66d4c6
macismail <samouel_77@hotmail.com>
2019-07-18 15:24:06 +0100

    change session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_email
+  before_action :set_detail
 
   protected
 
@@ -12,8 +12,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:f_name, :l_name, :email, :phone_n, :password, :current_password) }
   end
 
-  def set_email
-    session[:email] = current_user.email if user_signed_in?
-    @userEmail = session[:email]
+  def set_detail
+    if user_signed_in?
+      session[:f_name] = current_user.f_name
+      session[:l_name] = current_user.l_name
+      @userFullName = session[:f_name] + ' ' + session[:l_name]
+    end
   end
 end
